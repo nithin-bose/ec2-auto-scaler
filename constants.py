f152dbbdc8e5065dbf792883c82a657a3c49b2fd
Nithin Bose <nithin@localhost.localdomain>
2013-05-23 16:38:54 +0530

    modified default config path to example folder

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 APPLICATION_NAME = 'ec2-auto-scaler 0.1'
-DEFAULT_CONFIG_PATH = './config.ini'
+DEFAULT_CONFIG_PATH = './example/config.ini'
 DEFAULT_PROVIDER = 'aws'
 NON_CLUSTER_SECTIONS = ['main', 'aws', 'haproxy']
