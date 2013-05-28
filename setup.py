eccfc24e62f621bed43caffa51fc8fe8a6e7a298
Nithin Bose <nithin@localhost.localdomain>
2013-05-28 19:36:05 +0530

    Modified setup file to use the requirements file

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-setup(name='ec2-auto-scaler',
-      version='1.0',
-      description='An auto-scaling alternative to elastic load balancer in EC2',
-      author='Nithin Bose',
-      author_email='nithinbose@gmail.com',
-      url='https://github.com/nithinbose87/ec2-auto-scaler',
-      install_requires=['boto>=2.9.3', 'mako>=0.8.0', 'argparse>=1.2.1'],
-      license='MIT')
+with open('requirements.txt', 'r') as requirements:
+    required_packages = [p.strip() for p in requirements]
+
+setup(
+    name='ec2-auto-scaler',
+    version='1.0',
+    description='An auto-scaling alternative to elastic load balancer in EC2',
+    author='Nithin Bose',
+    author_email='nithinbose@gmail.com',
+    url='https://github.com/nithinbose87/ec2-auto-scaler',
+    install_requires=required_packages,
+    license='MIT'
+)
