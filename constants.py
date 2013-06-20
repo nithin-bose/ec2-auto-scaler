64f8e9115fcc558376800d0bda3b264c123d6d31
Nithin Bose <nithin@localhost.localdomain>
2013-06-20 06:47:35 +0530

    fixed default config path issue

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 APPLICATION_NAME = 'ec2-auto-scaler 0.1'
-DEFAULT_CONFIG_PATH = './example/config.ini'
+DEFAULT_CONFIG_PATH = 'example/config.ini'
 DEFAULT_PROVIDER = 'aws'
 NON_CLUSTER_SECTIONS = ['main', 'aws', 'haproxy']
 LOG_FORMAT = '%(asctime)s - %(levelname)s:%(name)s:%(message)s'
